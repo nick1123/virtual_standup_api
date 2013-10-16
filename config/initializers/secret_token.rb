f137e6ab6b13604bce9231c76ee3bfbe1f354f20
Nick Stewart <nicholas4@gmail.com>
2013-10-16 01:30:02 +0000

    Add basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VirtualStandupApi::Application.config.secret_key_base = '8b22aab9855ada5f037b3d7f1ec74dde32fe09b76e229fcc4102f187703e6ec36c769e0399abb5d800982acc8aa17ccb89503ff797c1b60bb57a49eca852f958'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VirtualStandupApi::Application.config.session_store :cookie_store, key: '_VirtualStandupApi_session'
